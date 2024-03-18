1578539cf7bea74d97eda015a00f0730dd31bd2c
Victoria Versteegh <120139396+tori-vers@users.noreply.github.com>
2024-03-18 18:46:51 -0400

    Update main.py with saved database

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scikit import transform
-from PIL import Image
+from PIL import Image, ImageFilter, ImageDraw
 import os
 import random
 import numpy as np
 
-def warp_image(image, keypoints, warp_strength=0.1):
+def warp_image(image, keypoints, warp_strength=0.1, blur_radius = 2):
  
     width, height = image.size
     new_keypoints = [(x + random.uniform(-warp_strength, warp_strength) * width, 
                       y + random.uniform(-warp_strength, warp_strength) * height) 
                      for x, y in keypoints]
     
-    # matrix 1
-    src = np.array(keypoints)
-    dst = np.array(new_keypoints)
-    tform = transform.ProjectiveTransform()
-    tform.estimate(src, dst)
+    # face mapping
+    mask = Image.new('L', (width, height), 0)
+    draw = ImageDraw.Draw(mask)
+    draw.polygon(new_keypoints, fill=255, outline=255)  # Fill and outline the polygon
     
-    # Warping
-    warped_image = transform.warp(np.array(image), tform.inverse)
-    
-    return Image.fromarray((warped_image * 255).astype(np.uint8))
+    # blur within polygon
+    blurred_image = image.filter(ImageFilter.GaussianBlur(blur_radius))
+    warped_image = Image.composite(blurred_image, image, mask)  # Swap the order of images
 
-# Read from the CSV file
+    return warped_image
+
+# reading from faces.csv file
 df = pd.read_csv('faces.csv')
-# randomize 
-df = df.sample(frac=1).reset_index(drop=True)
+df = df.sample(frac=1).reset_index(drop=True) # randomizing (can remove)
 
-# Show first 10 images w/ "alterations"
-for index, row in df.head(10).iterrows():
+# Number of images to warp and save
+num_images_to_warp = 500
+
+# creating a directory to store warped images
+warped_images_dir = 'warped_images'
+if not os.path.exists(warped_images_dir):
+    os.makedirs(warped_images_dir)
+
+# Iterate over each row in the dataframe & limit to the num of images
+for index, row in df.head(num_images_to_warp).iterrows():
     image_name = row['image_name']
     width, height = row['width'], row['height']
     x0, y0, x1, y1 = row['x0'], row['y0'], row['x1'], row['y1']
@@ -44,12 +51,8 @@ for index, row in df.head(10).iterrows():
     keypoints = [(x0, y0), (x1, y0), (x1, y1), (x0, y1)]
     warped_image = warp_image(image, keypoints)
 
-    # Show original & warped 
-    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-    axes[0].imshow(image)
-    axes[0].set_title('Original')
-    axes[1].imshow(warped_image)
-    axes[1].set_title('Warped')
+    # Save warped image
+    warped_image_path = os.path.join(warped_images_dir, f'warped_{image_name}')
+    warped_image.save(warped_image_path)
 
-    plt.show()
-
+    print(f"Warped image saved: {warped_image_path}")
